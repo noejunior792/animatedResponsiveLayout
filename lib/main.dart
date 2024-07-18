e9b179fe0d04c9fb11818939cc8c65174417a8d1
Noé Júnior <noedombaxe@gmail.com>
2024-07-18 20:56:24 +0100

    ✨feat: add a navigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'destinations.dart';
 import 'models/data.dart' as data;
 import 'models/models.dart';
 import 'widgets/email_list_view.dart';
@@ -37,12 +38,19 @@ class _FeedState extends State<Feed> {
   late final _backgroundColor = Color.alphaBlend(
       _colorScheme.primary.withOpacity(0.14), _colorScheme.surface);
 
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
         color: _backgroundColor,
         child: EmailListView(
+          selectedIndex: selectedIndex,
+          onSelected: (index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
           currentUser: widget.currentUser,
         ),
       ),
@@ -51,6 +59,22 @@ class _FeedState extends State<Feed> {
         foregroundColor: _colorScheme.onTertiaryContainer,
         onPressed: () {},
         child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: NavigationBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        destinations: destinations.map<NavigationDestination>((d) {
+          return NavigationDestination(
+            icon: Icon(d.icon),
+            label: d.label,
+          );
+        }).toList(),
+        selectedIndex: selectedIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
       ),
     );
   }
