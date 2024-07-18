28ab298959d6353ad9dbb3c5a8b92592688b8237
Noé Júnior <noedombaxe@gmail.com>
2024-07-18 20:32:33 +0100

    🔥feat: make a messanger app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'models/data.dart' as data;
+import 'models/models.dart';
+import 'widgets/email_list_view.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,11 +13,44 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData.light(useMaterial3: true),
+      home: Feed(currentUser: data.user_0),
+    );
+  }
+}
+
+class Feed extends StatefulWidget {
+  const Feed({
+    super.key,
+    required this.currentUser,
+  });
+
+  final User currentUser;
+
+  @override
+  State<Feed> createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+  late final _colorScheme = Theme.of(context).colorScheme;
+  late final _backgroundColor = Color.alphaBlend(
+      _colorScheme.primary.withOpacity(0.14), _colorScheme.surface);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: _backgroundColor,
+        child: EmailListView(
+          currentUser: widget.currentUser,
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: _colorScheme.tertiaryContainer,
+        foregroundColor: _colorScheme.onTertiaryContainer,
+        onPressed: () {},
+        child: const Icon(Icons.add),
       ),
     );
   }
